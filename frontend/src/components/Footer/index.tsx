bc1563887792c996f48a935a733ed71024c87923
DeboraMoraes <moraes.debora.dbora@gmail.com>
2021-05-05 17:05:16 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,34 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          Developed by{" "}
+          <a
+            href="https://github.com/DeboraMoraes"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Debora Moraes
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{" "}
+            <a
+              href="https://instagram.com/devsuperior.ig"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
